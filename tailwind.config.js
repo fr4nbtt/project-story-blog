3e368d81fa32d20d23cbbbcc5fdca7ee074902cc
fr4nbtt <batet.francisco@gmail.com>
2024-12-30 21:11:01 -0300

    feat: Create blog basic mobile skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deepForest: "hsl(167, 64%, 15%)",
+        forestGreen: "hsl(160, 25%, 35%)",
+        sageGray: "hsl(100, 12%, 70%)",
+        lightSun: "hsl(60, 100%, 95%)",
+      },
+      fontFamily: {
+        sans: ["Raleway", "system-ui"],
+        serif: ["Merriweather", "Georgia"],
+      },
+    },
   },
   plugins: [],
 };
